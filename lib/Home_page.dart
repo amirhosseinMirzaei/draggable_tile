84688c620fb591a48d6064f1b53488160fae44c3
AmirHossein <amirhossein1mirzaei79@gmail.com>
2025-09-06 00:06:16 -0700

    feat(grid): implement GridPainter to draw visual guide lines behind tiles

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'draggable_tile.dart';
+import 'grid_painter.dart';
 import 'models/tile_model.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,29 +11,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<TileModel> tiles = [
+  final List<TileModel> _tiles = [
     TileModel(id: 1, x: 0, y: 0, width: 120, height: 120, color: Colors.red),
     TileModel(id: 2, x: 140, y: 0, width: 120, height: 120, color: Colors.blue),
     TileModel(id: 3, x: 0, y: 140, width: 120, height: 120, color: Colors.green),
   ];
 
+  void _updateTile(TileModel updatedTile) {
+    setState(() {
+      final index = _tiles.indexWhere((t) => t.id == updatedTile.id);
+      if (index != -1) {
+        _tiles[index] = updatedTile;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children:
-            tiles.map((tile) {
-              return DraggableTile(
-                key: ValueKey(tile.id),
-                tile: tile,
-                onUpdate: (updatedTile) {
-                  setState(() {
-                    final index = tiles.indexWhere((t) => t.id == updatedTile.id);
-                    tiles[index] = updatedTile;
-                  });
-                },
-              );
-            }).toList(),
+      body: CustomPaint(
+        painter: GridPainter(cellSize: 120),
+        child: Stack(
+          children:
+              _tiles.map((tile) {
+                return DraggableTile(key: ValueKey(tile.id), tile: tile, onUpdate: _updateTile);
+              }).toList(),
+        ),
       ),
     );
   }
---
+++ lib/grid_painter.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class GridPainter extends CustomPainter {
+  final double cellSize;
+
+  GridPainter({this.cellSize = 100});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.grey.withOpacity(0.3)
+      ..strokeWidth = 1;
+
+    for (double x = 0; x < size.width; x += cellSize) {
+      canvas.drawLine(Offset(x, 0), Offset(x, size.height), paint);
+    }
+    for (double y = 0; y < size.height; y += cellSize) {
+      canvas.drawLine(Offset(0, y), Offset(size.width, y), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
