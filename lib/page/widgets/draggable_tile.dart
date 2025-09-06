bbdff83fbdb5be293a64b304afffea7a7e7f9694
AmirHossein <amirhossein1mirzaei79@gmail.com>
2025-09-06 00:44:30 -0700

    perf(tile): wrap DraggableTile with RepaintBoundary for smoother drag

--- lib/page/widgets/draggable_tile.dart
+++ lib/page/widgets/draggable_tile.dart
@@ -2,18 +2,12 @@ import 'package:flutter/material.dart';
 
 import '../../models/tile_model.dart';
 
-
 class DraggableTile extends StatefulWidget {
   final TileModel tile;
   final Function(TileModel) onUpdate;
   final Function(TileModel) onBringToFront;
 
-  const DraggableTile({
-    super.key,
-    required this.tile,
-    required this.onUpdate,
-    required this.onBringToFront,
-  });
+  const DraggableTile({super.key, required this.tile, required this.onUpdate, required this.onBringToFront});
 
   @override
   State<DraggableTile> createState() => _DraggableTileState();
@@ -40,51 +34,40 @@ class _DraggableTileState extends State<DraggableTile> {
     return Positioned(
       left: x,
       top: y,
-      child: GestureDetector(
-        onPanStart: (_) {
-          widget.onBringToFront(widget.tile);
-        },
-        onPanUpdate: (details) {
-          setState(() {
-            x += details.delta.dx;
-            y += details.delta.dy;
-          });
-          _updateTile();
-        },
-        child: Stack(
-          children: [
-            Container(
-              width: width,
-              height: height,
-              decoration: BoxDecoration(
-                color: widget.tile.color,
-                borderRadius: BorderRadius.circular(12),
-              ),
-            ),
-            Positioned(
-              right: 0,
-              bottom: 0,
-              child: GestureDetector(
-                onPanUpdate: (details) {
-                  setState(() {
-                    width += details.delta.dx;
-                    height += details.delta.dy;
-                    if (width < 60) width = 60;
-                    if (height < 60) height = 60;
-                  });
-                  _updateTile();
-                },
-                child: Container(
-                  width: 20,
-                  height: 20,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
+      child: RepaintBoundary(
+        // for better performance
+        child: GestureDetector(
+          onPanStart: (_) {
+            widget.onBringToFront(widget.tile);
+          },
+          onPanUpdate: (details) {
+            setState(() {
+              x += details.delta.dx;
+              y += details.delta.dy;
+            });
+            _updateTile();
+          },
+          child: Stack(
+            children: [
+              Container(width: width, height: height, decoration: BoxDecoration(color: widget.tile.color, borderRadius: BorderRadius.circular(12))),
+              Positioned(
+                right: 0,
+                bottom: 0,
+                child: GestureDetector(
+                  onPanUpdate: (details) {
+                    setState(() {
+                      width += details.delta.dx;
+                      height += details.delta.dy;
+                      if (width < 60) width = 60;
+                      if (height < 60) height = 60;
+                    });
+                    _updateTile();
+                  },
+                  child: Container(width: 20, height: 20, decoration: const BoxDecoration(color: Colors.white, shape: BoxShape.circle)),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
